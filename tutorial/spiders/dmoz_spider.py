765f16db5c015d4b5e3e672018ea40b31ddc7d6a
Kumaran Rajendhiran <kumaranvpl@gmail.com>
2016-03-16 22:52:46 +0530

    Following Links

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -6,11 +6,17 @@ class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
+        #"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        #"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
 
     def parse(self, response):
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
         for sel in response.xpath('//ul/li'):
             item = DmozItem()
             item['title'] = sel.xpath('a/text()').extract()
